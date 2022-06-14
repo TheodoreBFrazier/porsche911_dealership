5a09c1c15225acb23c7818e7ec0fa7f01039b56c
TheodoreBFrazier <theo.frazier@icloud.com>
2022-06-14 16:07:00 -0400

    Created an APP.JS file

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+// Installation of Dependencies //
+
+//CORS
+const cors = require(cors);
+//EXPRESS
+const express = require("express");
+
+//Configure & evoking APP to express
+const app = express();
+
+//Applicaton of Middleware
+app.use(cors());
+
+//Parse incoming JSON
+app.use(express.json());
+
+require("dotenv").config(); //Loads Environmental Variables 
+
+app.get("/", (request, response) => {
+    response.send("Welcome to the Porsche 911 Exclusive Dealership");
+})
+
+app.get("*", (request, response) => {
+    response.status(404).send("Page not found.")
+});
+
+//EXPORTING THE APPLICATION
+module.exports = app;
+
