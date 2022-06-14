9d021528f40aede97f8f6db172b398428ef287e2
TheodoreBFrazier <theo.frazier@icloud.com>
2022-06-14 16:18:09 -0400

    Beginning of table for the 992 models

---
+++ backend/db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS data_dev;
+CREATE DATABASE data_dev;
+
+\c data_dev
+
+--data for the current Porsche generation, the 992 
+CREATE TABLE PorscheModels992 (
+    model_id SERIAL PRIMARY KEY,
+    model TEXT,
+    body_style TEXT,
+    price INT,
+    power_in_hp INT,
+    zero_to_sixty INT,
+    description TEXT,
+)
