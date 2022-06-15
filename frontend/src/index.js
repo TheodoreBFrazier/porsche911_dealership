415a37898fb76c3f3147ae0d47624219298be2d4
TheodoreBFrazier <theo.frazier@icloud.com>
2022-06-14 23:25:15 -0400

    Made a home route on the homepage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//Importing of Browswer Router
-import { BrowserRouter } from "react-router-dom"
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,9 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+
     <App />
-    </BrowserRouter>
+
   </React.StrictMode>
 );
 
