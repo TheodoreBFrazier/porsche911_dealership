43bfe0abcee00a63b150639d91d312ca5d34e629
TheodoreBFrazier <theo.frazier@icloud.com>
2022-06-16 23:30:18 -0400

    Some very basic CSS for sections

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
-import herohome from "../images/heroimg.jpg";
 
 function Home() {
   return (
     <div className="Home">
-      <div className="Hero" style={{ backgroundImage: `url(${herohome})` }}>
-        <h1>This is the home page!!!</h1>
+      <div className="Hero">
+        <img src="https://images.hdqwalls.com/wallpapers/porsche-911-carrera-s-2019-rear-ek.jpg" alt="911-hero" />
+      </div>
+      <div className="Home-intro">
+        <h1>HI this is the intro.</h1>
+      </div>
+      <div className="Current-model-922">
+        <h1>922</h1>
       </div>
     </div>
   );
