4901ae7e47abfc6018f49ac09048de61364322af
TheodoreBFrazier <theo.frazier@icloud.com>
2022-06-18 17:26:41 -0400

    Two section column for CTA section

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -62,9 +62,20 @@ function Home() {
       </section>
 
       <section className="Home-cta">
-        <div className="cta-row">
-          <div className="column-cta-1">First Column</div>
-          <div className="column-cta-1">Second Column</div>
+        <div className="column-cta">
+          <img
+            src="https://assets.whichcar.com.au/image/upload/s--tPCIOKFH--/c_fill,q_auto:good/c_scale,w_3072/f_auto/v1/archive/whichcar/2018/11/28/-1/Seven-generations-of-the-Porsche-911-in-pictures.jpg"
+            alt="all gens"
+            className="generations-picture"
+          />
+        </div>
+
+        <div className="column-cta">
+<img
+src="https://newsroom.porsche.com/.imaging/mte/porsche-templating-theme/image_1290x726/dam/pnr/porsche_newsroom/Technik/Schl%C3%BCsselfertig_CPM/Schluesselfertig_991/jcr:content/Schluesselfertig_991.jpg"
+alt="Porsche and key"
+className="porsche"
+/>
         </div>
       </section>
     </div>
