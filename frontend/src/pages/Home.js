6390f22c7f0b9b78abefebc40034f01c70d31b6b
TheodoreBFrazier <theo.frazier@icloud.com>
2022-06-20 17:32:45 -0400

    Fixed issue of Hero text ad image not scrolling by making entire section RELATIVE position - now need to fix layout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,13 +6,18 @@ function Home() {
   return (
     <div className="Home">
       <section className="Hero">
+
+      <h1 className="Hero-H1">
+        It's time.
+      </h1>
+
+      
         <div className="Hero-image">
           <img
             src="https://images.hdqwalls.com/wallpapers/porsche-911-carrera-s-2019-rear-ek.jpg"
             alt="911-hero"
           />
 
-            <h1 className="Hero-H1">It's time.</h1>
 
         </div>
       </section>
