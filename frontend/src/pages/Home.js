d813a6def62cb09fd2894479dfc9bc8fab4180d1
TheodoreBFrazier <theo.frazier@icloud.com>
2022-06-20 17:42:47 -0400

    Z index sucessful mmoved hero text above immage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,19 +6,13 @@ function Home() {
   return (
     <div className="Home">
       <section className="Hero">
+        <h1 className="Hero-H1">It's time.</h1>
 
-      <h1 className="Hero-H1">
-        It's time.
-      </h1>
-
-      
         <div className="Hero-image">
           <img
             src="https://images.hdqwalls.com/wallpapers/porsche-911-carrera-s-2019-rear-ek.jpg"
             alt="911-hero"
           />
-
-
         </div>
       </section>
 
