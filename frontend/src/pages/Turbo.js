860b7d3fef76c59671f8f3ce10f901ec651c5603
TheodoreBFrazier <theo.frazier@icloud.com>
2022-06-20 23:33:24 -0400

    Resized the photos of the model range for the Turbo page -> 400 px

--- frontend/src/pages/Turbo.js
+++ frontend/src/pages/Turbo.js
@@ -2,11 +2,15 @@ import React from "react";
 
 //image imports
 
-
-
+// import the side profile photos of the 992 Turbo model range
+//Standard Turbo
 import turbo992 from "../Assets/images/porsche-model-18.png";
-
-
+//Turbo S
+import turbo992s from "../Assets/images/porsche-model-19.png";
+//Standard Turbo Cab
+import turbo992cab from "../Assets/images/porsche-model-21.png";
+//Turbo S Cab
+import turbo992scab from "../Assets/images/porsche-model-20.png";
 
 function TurboPage() {
   return (
@@ -28,8 +32,16 @@ function TurboPage() {
           of a modern 911. Choose from the regular Turbo, or the more
           exhilerating Turbo S.
         </h6>
- <img src={turbo992} alt="porsche" />
 
+        <div className="the-range-turbo">
+          <img src={turbo992} alt="porsche" />
+
+          <img src={turbo992s} alt="turbo-S" />
+
+          <img src={turbo992cab} alt="turbo-cab" />
+
+          <img src={turbo992scab} alt="turbo-s-cab" />
+        </div>
       </div>
     </div>
   );
