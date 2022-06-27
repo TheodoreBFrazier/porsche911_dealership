2ede54fc9b3b727eeceb77a41aefdb0e00b6b7f2
TheodoreBFrazier <theo.frazier@icloud.com>
2022-06-27 14:08:47 -0400

    Adding the data for the current 992 generation models to the seed

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -2,15 +2,15 @@
 
 INSERT INTO carrera_models_992(model_name, price, power_in_hp, top_speed, zero_to_sixty, description) VALUES
 ('Carrera Coupe', 106100, 379, 4.0),
-('Carrera Cabriolet', 118900, 379, 4.2),
+('Carrera Cabriolet', 118900, 379, 4.2, 'The open-top variant of the 911 heightens the 911 experience by adding freedom and fresh air. When closed, the Cabriolet maintains the same clean lines of the coupe models.'),
 ('Carrera 4 Coupe', 113400, 379, 4.0),
 ('Careera 4 Cabriolet' 126200, 379, 4.2),
 ('Carrera S Coupe', 123000, 443, 3.5),
 ('Carrera S Cabriolet', 135800, 443, 3.7),
 ('Carrera 4S Coupe', 130300, 443, 3.4),
 ('Carrera 4S Cabriolet', 143100, 443, 3.6),
-('Targa 4 Targa', 126200, 379, 4.2),
-('Targas 4S Targa', 143100, 443, 3.6),
+('Targa 4 Targa', 126200, 379, , 4.2, 'Arguably the most stylish 911 model. The Targa incorperates the characteristic rollbar with an innovative roof concept. A future classic that combines the experience of an open tpp with the comfort and safety of an enclsoed car.'),
+('Targas 4S Targa', 143100, 443, 3.6) 'The style and experience of the iconic Porsche Targa, with the added traction and security of four wheel drive.',
 ('GTS Coupe', 142600, 473, 3.2),
 ('GTS Cabriolet', 155400, 473, 3.4),
 ('Carrera 4 GTS Coupe', 149900, 473, 3.1),
@@ -18,7 +18,11 @@ INSERT INTO carrera_models_992(model_name, price, power_in_hp, top_speed, zero_t
 ('Targas 4 GTS Targa', 162700, 473, 3.3);
 
 INSERT INTO turbo_models_992(model_name, price, power_in_hp, top_speed, zero_to_sixty, description) VALUES
-('Turbo Coupe', 182900, ),
-('Turbo S Coupe',),
-('Turbo Cabriolet'),
-('Turbo S Cabriolet');
+('Turbo Coupe', 182900, 572, 2.7, ),
+('Turbo S Coupe', 216100, 640, 2.6, ),
+('Turbo Cabriolet' 195700, 572, , 2.8, ),
+('Turbo S Cabriolet' 228900, 640, 2.7, );
+
+INSERT INTO gt3_models_992(model_name, price, power_in_hp, top_speed, zero_to_sixty, description) VALUES
+('911 GT3', 169700, 502, 199, 3.2, 'The only 992-generation 911 that does not have a turbocharger, the GT3 brings 502 horses of naturally-aspirated joy. The 911 GT3 is not a daily driver, but you will want to drive it every day.'),
+('911 GT3 with Touring Package', 169700, 502, 199, 3.7, 'The 911 GT3 with Touring Package also sets the performance tone - but with focus primarily on the road. Thanks to ingenious areodynamics, cornering remains impressive, even without a large rear wing.');
