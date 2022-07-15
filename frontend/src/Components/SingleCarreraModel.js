54bcfd219537ad33de452a3a6c4dbabc744bbe8e
TheodoreBFrazier <theo.frazier@icloud.com>
2022-07-15 12:40:22 -0400

    Seeded database after adding image of Base model Carrera - about to test

--- frontend/src/Components/SingleCarreraModel.js
+++ frontend/src/Components/SingleCarreraModel.js
@@ -3,6 +3,7 @@ import React from "react";
 function SingleCarreraModel ({ carrera }) {
     return (
         <div className="turbo-card">
+            <div>{carrera.model_image}</div>
             <h2>{carrera.model_name}</h2>
             <p>Starting at ${carrera.price.toLocaleString()}</p>
         </div>
