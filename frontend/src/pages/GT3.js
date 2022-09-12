4173040bf54f976e1692715e00c447478609d7f7
TheodoreBFrazier <TheodoreBFrazier>
2022-09-12 15:18:47 -0400

    Added a hero image to GT3 page

--- frontend/src/pages/GT3.js
+++ frontend/src/pages/GT3.js
@@ -1,11 +1,18 @@
 import React from "react";
 
+import gt3Hero from "../Assets/Images/gt3-and-gt3-touring.jpg"
+
 import Gt3Pngs from "../Components/Gt3ModelsPng.js";
 import GT3ModelRange from "../Components/GT3ModelStats";
 
 export default function GT3Page() {
   return (
     <div>
+            <section className="gt3-hero">
+        <div className="gt3-page-hero">
+          <img src={gt3Hero} alt="GT3 and GT3 Touring Together"></img>
+        </div>
+      </section>
       <div className="gt3-carousel-buttons">
         <button>
           <h5>ALL MODELS</h5>
