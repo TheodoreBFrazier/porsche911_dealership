30efeb7b93b276484a4bc0e00488863f6d3f6445
TheodoreBFrazier <TheodoreBFrazier>
2022-09-13 14:57:28 -0400

    Got Text to overlay GT3 section - needs cleaning up

--- frontend/src/pages/GT3.js
+++ frontend/src/pages/GT3.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import gt3Hero from "../Assets/Images/gt3-and-gt3-touring.jpg"
 import fastGt3 from "../Assets/Images/gt3-at-speed.jpg"
+import fastGt3Touring from "../Assets/Images/gt3-touring.jpg"
 
 import Gt3Pngs from "../Components/Gt3ModelsPng.js";
 import GT3ModelRange from "../Components/GT3ModelStats";
@@ -15,25 +16,49 @@ export default function GT3Page() {
         </div>
       </section>
 
-      <section className="gt3-intro">
-        <img src={fastGt3} alt="gt3 at speed" />
-        <h5></h5>
-      </section>
 
-      <section className="gt3-intro-text">
+
+      <section className="gt3">
+
+        <div className="gt3-intro">
+          <img src={fastGt3} alt="gt3 at speed" />
+        </div>
+
+
         <div className="text-section">
           <h2>Go. <div className="word-fast"> Fast. </div> </h2>
-          <p> Carpe diem, they say – seize the day. But we are Porsche. And so our motto for the new 911 GT3 models is: carpe secundum – get the most out of every second. 
+          <p> Carpe diem, they say – seize the day. But we are Porsche. And so our motto for the new 911 GT3 models is: carpe secundum –
+            get the most out of every second.
             <br />
             <br />
-            We know that your time is precious. And every minute in the cockpit is valuable. So, we have once again intensified everything that makes a sports car: performance, tuning, aerodynamics.
+            We know that your time is precious. And every minute in the cockpit is valuable.
+            So, we have once again intensified everything that makes a sports car: performance, tuning, aerodynamics.
             <br />
-            <br /> 
+            <br />
             Time flies. This is your time. Hop in.</p>
         </div>
       </section>
 
-      <section className="gt3"
+      <section className="gt3-touring-intro">
+        <img src={fastGt3Touring} alt="gt3 touring at speed" />
+        <h5></h5>
+      </section>
+
+      <section className="gt3-touring-intro-text">
+        <div className="text-section">
+          <h2>Go. <div className="word-fast"> Fast. </div> </h2>
+          <p> Carpe diem, they say – seize the day. But we are Porsche. And so our motto for the new 911 GT3 models is: carpe secundum –
+            get the most out of every second.
+            <br />
+            <br />
+            We know that your time is precious. And every minute in the cockpit is valuable.
+            So, we have once again intensified everything that makes a sports car: performance, tuning, aerodynamics.
+            <br />
+            <br />
+            Time flies. This is your time. Hop in.</p>
+        </div>
+      </section>
+
 
       <div className="gt3-carousel-buttons">
         <button>
