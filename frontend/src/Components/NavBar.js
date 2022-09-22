c5dc4b59793c50ac7b847100108272cdf80122bd
TheodoreBFrazier <TheodoreBFrazier>
2022-09-21 20:50:03 -0400

    Building dropdown menu for the Carrera model

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom"
 import logoMenu from "../Assets/Images/porsche_logo.png"
 import sellIcon from "../Assets/Images/porsche-sell-icon.png"
 
+import Dropdown from "react-bootstrap/Dropdown"
+
 
 function NavBar() {
     return (
@@ -24,15 +26,18 @@ function NavBar() {
                 </li>
 
 
+                <Dropdown>
+                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                        Dropdown Button
+                    </Dropdown.Toggle>
 
-                <li>
+                    <Dropdown.Menu>
                     <Link to="/carrera" style={{ textDecoration: 'none' }}>
                         <h5>
                             The New Carrera
                         </h5>
                     </Link>
-                </li>
-                <li>
+
 
                     <Link to="/turbo" style={{ textDecoration: 'none' }}>
                         <h5>
@@ -40,23 +45,24 @@ function NavBar() {
                         </h5>
                     </Link>
 
-                </li>
-                <li>
 
                     <Link to="/gt3" style={{ textDecoration: 'none' }}>
                         <h5>
                             GT3
                         </h5>
                     </Link>
+                    </Dropdown.Menu>
 
-                </li>
-                <li>
-                    <Link to="/generations" style={{ textDecoration: 'none' }}>
-                        <h5>
-                            generations
-                        </h5>
-                    </Link>
-                </li>
+
+                </Dropdown>
+
+                <Link to="/generations" style={{ textDecoration: 'none' }}>
+                    <h5>
+                        generations
+                    </h5>
+                </Link>
+
+
 
                 <li>
                     <div className='sell-icon'>
