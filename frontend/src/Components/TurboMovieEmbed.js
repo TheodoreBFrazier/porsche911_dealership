dbbb9b5d902e206a3a02e3f17e2800fb18c0365e
TheodoreBFrazier <TheodoreBFrazier>
2022-09-27 14:00:50 -0400

    Removed controls and youtube branding so video looks much cleaner

--- frontend/src/Components/TurboMovieEmbed.js
+++ frontend/src/Components/TurboMovieEmbed.js
@@ -6,11 +6,12 @@ const TurboMovie = ({ embededMovieId }) => (
         <iframe
             width="100vw"
             height="100vh"
-            src={`https://www.youtube.com/embed/${embededMovieId}`}
+            src={`https://www.youtube.com/embed/${embededMovieId}?autoplay=1&showinfo=0&controls=0"`}
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encyrpted-media; gyroscope: picture-in-picture"
             allowFullScreen
             title="Turbo Promo"
+            autoplay="0"
         />
     </div>
 );
