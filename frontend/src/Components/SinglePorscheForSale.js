65f62b1cc0d23f578efb95a239ecb3e1d6d970d2
TheodoreBFrazier <TheodoreBFrazier>
2022-09-28 14:16:41 -0400

    CSS on for sale Porsches

--- frontend/src/Components/SinglePorscheForSale.js
+++ frontend/src/Components/SinglePorscheForSale.js
@@ -4,15 +4,20 @@ export default function SingleCarForSale({ porsche }) {
 
     return (
         <div className="single-car-card">
-            <h3>{porsche.year}  {porsche.model_name} </h3>
-            <h6>${porsche.price.toLocaleString()}</h6>
+            <div className="car-heading">
+                <h4>{porsche.year}  {porsche.model_name} </h4>
+            </div>
+            <div className="car-subheading">
+                <p>{porsche.color} - {porsche.transmission}</p>
+                <h3>${porsche.price.toLocaleString()}</h3>
+            </div>
             <div className="forsale-model-image">
                 <img src={`/images/${porsche.main_image}`}
                     alt="for sale porsche main photo"
                 />
             </div>
         </div>
-    )
+    );
 
 }
 
