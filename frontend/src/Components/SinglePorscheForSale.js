803723e75c425afac31c658dcdda43de9ab2e8b7
TheodoreBFrazier <TheodoreBFrazier>
2022-09-28 13:39:13 -0400

    Removed the bodystyle from the SinglePorscheForSale so body style doesn't appear twice, since it's in the model name already

--- frontend/src/Components/SinglePorscheForSale.js
+++ frontend/src/Components/SinglePorscheForSale.js
@@ -4,7 +4,7 @@ export default function SingleCarForSale({ porsche }) {
 
     return (
         <div className="single-car-card">
-            <h3>{porsche.year}  {porsche.model_name} {porsche.body_style}</h3>
+            <h3>{porsche.year}  {porsche.model_name} </h3>
             <h6>${porsche.price.toLocaleString()}</h6>
             <div className="forsale-model-image">
                 <img src={`/images/${porsche.main_image}`}
