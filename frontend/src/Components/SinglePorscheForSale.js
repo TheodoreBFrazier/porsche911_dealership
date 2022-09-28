fb75987265185888baebc583b1196470e156c71a
TheodoreBFrazier <TheodoreBFrazier>
2022-09-28 14:58:05 -0400

    Making a CTA button to view single car

--- frontend/src/Components/SinglePorscheForSale.js
+++ frontend/src/Components/SinglePorscheForSale.js
@@ -5,20 +5,25 @@ export default function SingleCarForSale({ porsche }) {
 
     return (
         <div className="single-car-card">
-            <Link to={`/porschestore/${porsche.porsche_id}`}>
-                <div className="car-heading">
-                    <h4>{porsche.year}  {porsche.model_name} </h4>
-                </div>
-                <div className="car-subheading">
-                    <p>{porsche.color} - {porsche.transmission}</p>
-                    <h3>${porsche.price.toLocaleString()}</h3>
-                </div>
-                <div className="forsale-model-image">
-                    <img src={`/images/${porsche.main_image}`}
-                        alt="for sale porsche main photo"
-                    />
-                </div>
-            </Link>
+
+            <div className="car-heading">
+                <h4>{porsche.year}  {porsche.model_name} </h4>
+            </div>
+            <div className="car-subheading">
+                <p>{porsche.color} - {porsche.transmission}</p>
+                <h3>${porsche.price.toLocaleString()}</h3>
+            </div>
+
+            <div className="forsale-model-image">
+                <img src={`/images/${porsche.main_image}`}
+                    alt="for sale porsche main photo"
+                />
+            </div>
+            <button>
+                <Link to={`/porschestore/${porsche.porsche_id}`} style={{ textDecoration: 'none' }}>
+BUTTON
+                </Link>
+            </button>
         </div>
     );
 
