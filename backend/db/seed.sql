09ed5d436dd13a1a71c90f03af3fbb998f68b8ff
TheodoreBFrazier <TheodoreBFrazier>
2022-10-01 15:08:14 -0400

    Added photos of the generations of the 911

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -819,47 +819,55 @@ VALUES
         '901',
         '1963',
         '1973',
-        'Designed as a larger, more practical version of the 356 with an extra pair of seats, the 901 was the beginning a icon. Early examples used a two-liter engine with only 130 horsepower, but the light weight of just over a ton made up for the lack of power. Engine output increased over the life of the production run, topping out with the 210 2.7 Carrera RS.'
+        'Designed as a larger, more practical version of the 356 with an extra pair of seats, the 901 was the beginning a icon. Early examples used a two-liter engine with only 130 horsepower, but the light weight of just over a ton made up for the lack of power. Engine output increased over the life of the production run, topping out with the 210 2.7 Carrera RS.',
+        'generations_first.webp'
     ),
     (
         'G-series',
         '1973',
         '1989',
-        'The G generation marked a major change, with a very discernable difference in styling, and technology. This generation ran for almost 20 years. The most unique move was the introduction of turbocharging, represented by the Turbo model which featured 260 horsepower.'
+        'The G generation marked a major change, with a very discernable difference in styling, and technology. This generation ran for almost 20 years. The most unique move was the introduction of turbocharging, represented by the Turbo model which featured 260 horsepower.',
+        'generations_G_series.webp'
     ),
     (
         '964',
         '1989',
         '1994',
-        'A mild syling change combined with tons of new tech, the 964 appeared to be very similar to the G series, but was 85 percent new. Four wheel drive arrived, along with power steering, ABS and airbags. Considered by many to be the least attractive 911, 964s could be found for relatively cheap on the classic market.'
+        'A mild syling change combined with tons of new tech, the 964 appeared to be very similar to the G series, but was 85 percent new. Four wheel drive arrived, along with power steering, ABS and airbags. Considered by many to be the least attractive 911, 964s could be found for relatively cheap on the classic market.',
+        'generations_964.webp'
     ),
     (
         '993',
         '1994',
         '1998',
-        'Considered by many to have the most beautiful and purest 911 design, the 993 also represented the end of an era, by being the last air-cooled 911. The GT2 version of the 993 began the pratice of ultra high-performance 911s like the GT3.'
+        'Considered by many to have the most beautiful and purest 911 design, the 993 also represented the end of an era, by being the last air-cooled 911. The GT2 version of the 993 began the pratice of ultra high-performance 911s like the GT3.',
+        'generations_993.webp'
     ),
     (
         '996',
         '1998',
         '2004',
-        'Undoubtably the most controversial 911, the 996 was derided by enthusiasts for "losing character" for the styling change to the headlights and use of a water-cooled engine, as well as the sharing of body panels with the cheaper Boxter model. However, the 996 is the 911 that saved Porsche, being the first mass-produced model. It marked the exponential growth in 911 variants, and the launch of the first ever GT3 variant.'
+        'Undoubtably the most controversial 911, the 996 was derided by enthusiasts for "losing character" for the styling change to the headlights and use of a water-cooled engine, as well as the sharing of body panels with the cheaper Boxter model. However, the 996 is the 911 that saved Porsche, being the first mass-produced model. It marked the exponential growth in 911 variants, and the launch of the first ever GT3 variant.',
+        'generations_996.webp'
     ),
     (
         '997',
         '2004',
         '2011',
-        'Round headlights made a return with the all-new 997. The 997 was the first 911 generation where all models had more than 300 horsepower, with a GT2 variant with 530 horspower. It was not without criticism though, with claims of "deminished character" revolving around steering feel and ironically, it being too "secure" in turns due to stronger grip.'
+        'Round headlights made a return with the all-new 997. The 997 was the first 911 generation where all models had more than 300 horsepower, with a GT2 variant with 530 horspower. It was not without criticism though, with claims of "deminished character" revolving around steering feel and ironically, it being too "secure" in turns due to stronger grip.',
+        'generations_977.webp'
     ),
     (
         '991',
         '2011',
         '2018',
-        'The 991 was considered one the best-looking 911s in decades, especially with the new Targa version. There were criticisms regarding the electric steering and the eventual use of turbo engines in almost all variants in the 991.2 version, but that did not stop solid sales for the 991, which included the sale of the one-millionth 911.'
+        'The 991 was considered one the best-looking 911s in decades, especially with the new Targa version. There were criticisms regarding the electric steering and the eventual use of turbo engines in almost all variants in the 991.2 version, but that did not stop solid sales for the 991, which included the sale of the one-millionth 911.',
+        'generations_991.webp'
     ),
     (
         '992',
         '2018',
         'Present',
-        'The current 992-generaton marked perhaps the most extreme styling chage for the 911, with a long light bar in the back and full track width for all models, not just the four-wheel drive and motorsport models. The turbocharged flat-six engines remain, with incredibly low emmssons, yet more power. The 992 shows that high performance cars can join the fight for climate change. With the 992, there are also talks of the first hybrid 911 becoming a reality.'
+        'The current 992-generaton marked perhaps the most extreme styling chage for the 911, with a long light bar in the back and full track width for all models, not just the four-wheel drive and motorsport models. The turbocharged flat-six engines remain, with incredibly low emmssons, yet more power. The 992 shows that high performance cars can join the fight for climate change. With the 992, there are also talks of the first hybrid 911 becoming a reality.',
+        'generations_992.webp'
     );
